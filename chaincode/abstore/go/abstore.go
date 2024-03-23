0997fe8948b481e87bdf44e1531d540cab93de60
hanjunnn <qwedfr79@naver.com>
2024-03-23 16:14:59 +0900

    add itemName

--- chaincode/abstore/go/abstore.go
+++ chaincode/abstore/go/abstore.go
@@ -1,19 +1,3 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -28,84 +12,86 @@ type ABstore struct {
 	contractapi.Contract
 }
 
-func (t *ABstore) Init(ctx contractapi.TransactionContextInterface, A string, Aval int, B string, Bval int, C string, Cval int) error {
+func (t *ABstore) Init(ctx contractapi.TransactionContextInterface, sell string, sellVal int, buy string, buyVal int, company string, companyVal int) error {
 	fmt.Println("ABstore Init")
 	var err error
 	// Initialize the chaincode
-	fmt.Printf("Aval = %d, Bval = %d\n, Cval = %d\n", Aval, Bval,Cval)
+	fmt.Printf("sellVal = %d, buyVal = %d, companyVal = %d\n", sellVal, buyVal, companyVal)
 	// Write the state to the ledger
-	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
+	err = ctx.GetStub().PutState(sell, []byte(strconv.Itoa(sellVal)))
 	if err != nil {
 		return err
 	}
 
-	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	err = ctx.GetStub().PutState(buy, []byte(strconv.Itoa(buyVal)))
 	if err != nil {
 		return err
 	}
 
-	err = ctx.GetStub().PutState(C, []byte(strconv.Itoa(Cval)))
+	err = ctx.GetStub().PutState(company, []byte(strconv.Itoa(companyVal)))
 	if err != nil {
 		return err
 	}
-
 
 	return nil
 }
 
-// Transaction makes payment of X units from A to B
-func (t *ABstore) Invoke(ctx contractapi.TransactionContextInterface, A, B, C string, X int) error {
+func (t *ABstore) Invoke(ctx contractapi.TransactionContextInterface, sell, buy, company, itemName string, X int) error {
 	var err error
-	var Aval int
-	var Bval int
-	var Cval int
+	var sellVal int
+	var buyVal int
+	var companyVal int
 	// Get the state from the ledger
 	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := ctx.GetStub().GetState(A)
+	sellValBytes, err := ctx.GetStub().GetState(sell)
 	if err != nil {
 		return fmt.Errorf("Failed to get state")
 	}
-	if Avalbytes == nil {
+	if sellValBytes == nil {
 		return fmt.Errorf("Entity not found")
 	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
+	sellVal, _ = strconv.Atoi(string(sellValBytes))
 
-	Bvalbytes, err := ctx.GetStub().GetState(B)
+	buyValBytes, err := ctx.GetStub().GetState(buy)
 	if err != nil {
 		return fmt.Errorf("Failed to get state")
 	}
-	if Bvalbytes == nil {
+	if buyValBytes == nil {
 		return fmt.Errorf("Entity not found")
 	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
+	buyVal, _ = strconv.Atoi(string(buyValBytes))
 
-	Cvalbytes, err := ctx.GetStub().GetState(C)
+	companyValBytes, err := ctx.GetStub().GetState(company)
 	if err != nil {
 		return fmt.Errorf("Failed to get state")
 	}
-	if Cvalbytes == nil {
+	if companyValBytes == nil {
 		return fmt.Errorf("Entity not found")
 	}
-	Cval, _ = strconv.Atoi(string(Cvalbytes))
+	companyVal, _ = strconv.Atoi(string(companyValBytes))
+	
+	// Calculate fee to be paid to the company
+	fee := int(float64(X) * 0.03)
 	
 	// Perform the execution
-	Aval = Aval - X
-	Bval = Bval + X - ( X / 10 )
-	Cval = Cval + ( X / 10 )
-	fmt.Printf("Aval = %d, Bval = %d, Cval = %d\n", Aval, Bval, Cval)
+	sellVal = sellVal-X-fee
+	buyVal += X
+	companyVal += fee
+	
+	fmt.Printf("sellVal = %d, buyVal = %d, companyVal = %d\n", sellVal, buyVal, companyVal)
 
 	// Write the state back to the ledger
-	err = ctx.GetStub().PutState(A, []byte(strconv.Itoa(Aval)))
+	err = ctx.GetStub().PutState(sell+"-"+itemName, []byte(strconv.Itoa(sellVal)))
 	if err != nil {
 		return err
 	}
 
-	err = ctx.GetStub().PutState(B, []byte(strconv.Itoa(Bval)))
+	err = ctx.GetStub().PutState(buy+"-"+itemName, []byte(strconv.Itoa(buyVal)))
 	if err != nil {
 		return err
 	}
 
-	err = ctx.GetStub().PutState(C, []byte(strconv.Itoa(Cval)))
+	err = ctx.GetStub().PutState(company+"-"+itemName, []byte(strconv.Itoa(companyVal)))
 	if err != nil {
 		return err
 	}
@@ -113,11 +99,11 @@ func (t *ABstore) Invoke(ctx contractapi.TransactionContextInterface, A, B, C st
 	return nil
 }
 
-// Delete  an entity from state
-func (t *ABstore) Delete(ctx contractapi.TransactionContextInterface, A string) error {
 
+// Delete an entity from state
+func (t *ABstore) Delete(ctx contractapi.TransactionContextInterface, sell string) error {
 	// Delete the key from the state in ledger
-	err := ctx.GetStub().DelState(A)
+	err := ctx.GetStub().DelState(sell)
 	if err != nil {
 		return fmt.Errorf("Failed to delete state")
 	}
@@ -126,41 +112,41 @@ func (t *ABstore) Delete(ctx contractapi.TransactionContextInterface, A string) 
 }
 
 // Query callback representing the query of a chaincode
-func (t *ABstore) Query(ctx contractapi.TransactionContextInterface, A string) (string, error) {
+func (t *ABstore) Query(ctx contractapi.TransactionContextInterface, sell string) (string, error) {
 	var err error
 	// Get the state from the ledger
-	Avalbytes, err := ctx.GetStub().GetState(A)
+	sellValBytes, err := ctx.GetStub().GetState(sell)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		jsonResp := "{\"Error\":\"Failed to get state for " + sell + "\"}"
 		return "", errors.New(jsonResp)
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+	if sellValBytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + sell + "\"}"
 		return "", errors.New(jsonResp)
 	}
 
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	jsonResp := "{\"Name\":\"" + sell + "\",\"Amount\":\"" + string(sellValBytes) + "\"}"
 	fmt.Printf("Query Response:%s\n", jsonResp)
-	return string(Avalbytes), nil
+	return string(sellValBytes), nil
 }
 
 func (t *ABstore) GetAllQuery(ctx contractapi.TransactionContextInterface) ([]string, error) {
-    resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
-    if err != nil {
-        return nil, err
-    }
-    defer resultsIterator.Close()
-    var wallet []string
-    for resultsIterator.HasNext() {
-        queryResponse, err := resultsIterator.Next()
-        if err != nil {
-            return nil, err
-        }
-        jsonResp := "{\"Name\":\"" + string(queryResponse.Key) + "\",\"Amount\":\"" + string(queryResponse.Value) + "\"}"
-        wallet = append(wallet, jsonResp)
-    }
-    return wallet, nil
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+	var wallet []string
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		jsonResp := "{\"Name\":\"" + string(queryResponse.Key) + "\",\"Amount\":\"" + string(queryResponse.Value) + "\"}"
+		wallet = append(wallet, jsonResp)
+	}
+	return wallet, nil
 }
 
 func main() {
